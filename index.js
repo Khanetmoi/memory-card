ece6d3b0e07b494005b5d96dcb40d3bb1f9db0d1
Khanetmoi <khanetmoi@gmail.com>
2023-03-07 03:46:00 +0000

    javascript added

---
+++ index.js
@@ -0,0 +1,16 @@
+// import 'library.js'
+
+const board = document.getElementById('board');
+
+
+
+
+
+// initialasation
+for(let i = 0; i<16; i++)
+{
+    board.innerHTML += `<div class="card"></div>`
+}
+
+
+
---
+++ library.js
@@ -0,0 +1,94 @@
+let cards = [
+    {
+        image: 'asset/bird1.jpg',
+        id: 1,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird2.jpg',
+        id: 2,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird3.jpg',
+        id: 3,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird4.jpg',
+        id: 4,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird5.jpg',
+        id: 5,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird6.jpg',
+        id: 6,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird7.jpg',
+        id: 7,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird8.jpg',
+        id: 8,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird9.jpg',
+        id: 9,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird10.jpg',
+        id: 10,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird11.jpg',
+        id: 11,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird12.jpg',
+        id: 12,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird13.jpg',
+        id: 13,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird14.jpg',
+        id: 14,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird15.jpg',
+        id: 15,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird16.jpg',
+        id: 16,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird17.jpg',
+        id: 17,
+        state: 'not found'
+    },
+    {
+        image: 'asset/bird18.jpg',
+        id: 18,
+        state: 'not found'
+    },
+];
+
+export default cards;
